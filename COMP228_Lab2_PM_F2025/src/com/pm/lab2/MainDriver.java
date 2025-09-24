49b9114feaee18d7e18873c6f2ebe46a4104980d
Beau <77253127+ieBeau@users.noreply.github.com>
2025-09-24 12:17:36 -0400

    Update MainDriver to allow input for 5 interests. Value changed on init commit.

--- COMP228_Lab2_PM_F2025/src/com/pm/lab2/MainDriver.java
+++ COMP228_Lab2_PM_F2025/src/com/pm/lab2/MainDriver.java
@@ -11,7 +11,7 @@ public class MainDriver {
 
         Interest[] interests = new Interest[5];
 
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 5; i++) {
             try {
 
                 System.out.println("\nEnter values for Interest " + (i + 1) + ": ");
